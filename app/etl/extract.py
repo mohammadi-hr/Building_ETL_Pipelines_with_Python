06595d14bee4528c0c3d63b5877556639942f4d6
hamidreza mohammadi <ir.mohammadi.hr@gmail.com>
2025-10-22 20:42:49 +0330

    feat: read data from db and webpage
    fetch table by pd_read_html

--- app/etl/extract.py
+++ app/etl/extract.py
@@ -3,7 +3,7 @@ import json
 import pandas as pd
 import certifi
 import urllib3
-
+import sqlite3
 
 
 BASE_DIR= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -57,4 +57,29 @@ def source_data_from_parquet(parquet_file_path):
         print(e)
     return df_parquet
 
-source_data_from_parquet(df_parquet)
+# source_data_from_parquet(df_parquet)
+db_path = os.path.join(DATA_DIR, "movies.sqlite")
+
+def source_data_from_table(db_name, table_name):
+    try:
+        with sqlite3.connect(db_name) as conn:
+            df_table = pd.read_sql(f"SELECT * FROM {table_name} ", con=conn)
+    except Exception as e:
+        df_table = pd.DataFrame()
+        print()
+    return df_table
+
+# print(source_data_from_table(db_path, "movies"))
+
+webpage_url= "https://www.tgju.org/profile/geram18"
+def source_data_from_html(html_url, matching_keywords):
+    try:
+        df_html = pd.read_html(html_url, match=matching_keywords)
+        df_html = df_html[0]
+    except Exception as e:
+        df_html = pd.DataFrame()
+        print(e)
+    return df_html
+
+# www.tgju.org دریافت نرخ قیمت طلا از جدول مربوطه در وب سایت
+print(source_data_from_html(webpage_url, 'خصیصه'))
