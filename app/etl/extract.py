18558b90809be8bdf399c9d02cebad133fdcc890
hamidreza mohammadi <ir.mohammadi.hr@gmail.com>
2025-10-22 19:38:00 +0330

    feat: read csv and parquet file

--- app/etl/extract.py
+++ app/etl/extract.py
@@ -9,15 +9,6 @@ import urllib3
 BASE_DIR= os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DATA_DIR = os.path.join(BASE_DIR, 'data', 'raw')
 
-csv_path = os.path.join(DATA_DIR, "h9gi-nx95.csv")
-parquet_path = os.path.join(DATA_DIR, "yellow_tripdata_2025-01.parquet")
-
-df_csv= pd.read_csv(csv_path)
-df_csv.head()
-
-df_parquet= pd.read_parquet(parquet_path)
-df_parquet.head()
-
 api_end_point= "https://jsonplaceholder.typicode.com/posts"
 
 def source_data_from_api(url):
@@ -37,4 +28,33 @@ def source_data_from_api(url):
         print(e)
     return pd_api
 
-print(source_data_from_api(api_end_point))
+# print(source_data_from_api(api_end_point))
+
+
+csv_path = os.path.join(DATA_DIR, "h9gi-nx95.csv")
+
+def source_data_from_csv(csv_file_path):
+    try:
+        df_csv = pd.read_csv(csv_file_path)
+    except Exception as e:
+        df_csv = pd.DataFrame()
+        print(e)
+    return df_csv
+
+# print(source_data_from_csv(csv_path))
+
+
+parquet_path = os.path.join(DATA_DIR, "yellow_tripdata_2025-01.parquet")
+
+
+df_parquet= pd.read_parquet(parquet_path)
+
+def source_data_from_parquet(parquet_file_path):
+    try:
+        df_parquet = pd.read_parquet(parquet_file_path)
+    except Exception as e:
+        df_parquet = pd.DataFrame()
+        print(e)
+    return df_parquet
+
+source_data_from_parquet(df_parquet)
